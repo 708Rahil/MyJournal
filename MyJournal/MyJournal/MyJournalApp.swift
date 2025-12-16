e670d253094871999dd437381f8324366828c84c
Rahil Gandhi <708rahil@gmail.com>
2025-12-16 18:06:30 -0500

    Initial Project

---
+++ MyJournal/MyJournal/MyJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJournalApp.swift
+//  MyJournal
+//
+//  Created by Rahil Gandhi on 2025-12-16.
+//
+
+import SwiftUI
+
+@main
+struct MyJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
